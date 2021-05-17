0db8606102ce831b976dc767915c912dbf524337
saim <saim@flask.com>
2021-05-17 05:04:20 +0000

    Update

--- application/routes.py
+++ application/routes.py
@@ -1,49 +1,44 @@
 from application import app, db
-from application.models import Rods 
-
-
+from application.models import Rods
+from flask import Flask, render_template, request, redirect, url_for
+from application.forms import TaskForm
 
 @app.route("/")
 @app.route("/home")
 def home():
-    all_tasks = tasks.query.all()
+    all_tasks = Rods.query.all()
     output = ""
-    return render_Web_Design("index.html, title=home, all_tasks=all_tasks")
+    return render_template("index.html, title=home, all_tasks=all_tasks")
 
-
-    #for task in all_tasks
-    #output += task.description + " - completed? " + str(task.completed) + "<br>"
-    #return output
-
-    @app.route("/create", methods=["GET","POST"])
-    def create():
+@app.route("/create", methods=["GET","POST"])
+def create():
     form = TaskForm() 
-    if request.method == "POST"
-        if form.validate_on_submit
-            new_task = Tasks(description=form.description.data)
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect(url_for("home"))
-     return render_template("add.html", title="Create a task", form=form)
+    if request.method == 'POST':
+     if form.validate_on_submit:
+        new_task = Rods(description=form.description.data)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add.html", title="Created Rod", form=form)
 
- @app.route("/complete/<int:id>")
+@app.route("/complete/<int:id>")
 def complete(id):
-    task = tasks.query.filter_by(id=id).first()
+    task = Rods.query.filter_by(id=id).first()
     task.completed = True 
     db.session.commit()
     return "Completed Task"
 
-    @app.route("/incomplete/<int:id>")
-    def incomplete(id)
-    task = tasks.query.filter_by(id=id).first()
+@app.route("/incomplete/<int:id>")
+def incomplete(id):
+    task = Rods.query.filter_by(id=id).first()
     task.completed - False
     db.session.commit()
     return "Incomplete Task"
 
 @app.route("/update/<int:id>", methods=["GET", "POST"])
-def update(id)
+def update(id):
     form = TaskForm()
-    task = Tasks.query.filter_by(id=id).first()
+    task = Rods.query.filter_by(id=id).first()
     if request.method == "POST": 
         task.description = form.description.data
         db.session.commit()
@@ -51,9 +46,8 @@ def update(id)
     return render_template("update.html", form=form, title="Update Task", task=task)
 
 @app.route("/delete/<int:id>", methods=["GET","POST"])
-def delete(id)
-    task = tasks.query.filter_by(id=id).first()
+def delete(id):
+    task = Rods.query.filter_by(id=id).first()
     db.session.delete(task)
     db.session.commit
     return redirect(url_for("home"))
-
